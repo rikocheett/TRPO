41cd68cfc53a9bc52e0e3bfa00648cab20ff63ce
rikocheett <01.sasha.01@mail.ru>
2021-04-09 23:55:15 +0300

    11 lab

--- index.php
+++ index.php
@@ -20,6 +20,12 @@ try
 {
     $SolutionOfAQuadraticEquation = new SolutionOfAQuadraticEquation();
     $res = $SolutionOfAQuadraticEquation->solve((float)$a, (float)$b, (float)$c);
+    if($a == 0 || empty($a)){
+        ZaytsevLog::log("Determined to be a linear equation" . PHP_EOL . "Equation root:" . PHP_EOL);
+    }
+    else{
+        ZaytsevLog::log("Determined to be a quadratic equation" . PHP_EOL . "Equation roots:" . PHP_EOL);
+    }
     foreach ($res as $el)
     {
         ZaytsevLog::log($el);
